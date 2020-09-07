24fc17fc56a228f3ed4f3843236e668a2919fb62
folsterbo <folsterbo@gmail.com>
2020-09-07 20:50:19 +0300

    after comments

--- 5_2_1_Math/js/script.js
+++ 5_2_1_Math/js/script.js
@@ -1,8 +1,8 @@
 function MyFunc(x) {
-  let n = Math.floor(Math.random()*(9-1))+1;
+  let n = Math.floor(Math.random()*(10-1))+1;
   let a = x**n;
   return Math.abs(a);
 }
 function My(x) {
-  return Math.abs(x**(Math.floor(Math.random()*(9-1))+1));
+  return Math.abs(x**(Math.floor(Math.random()*(10-1))+1));
 }
--- 5_6_1_Sin/js/script.js
+++ 5_6_1_Sin/js/script.js
@@ -3,7 +3,8 @@ const formSin = document.forms[0].elements.angle.value;
 const angle = Number(formSin);
 
 
+
 bSin.onsubmit = function(e) {
- e.preventDefault();
- document.forms[0].elements.total.value = 'sin(' + String(document.forms[0].elements.angle.value) + '°)=' + String(Math.sin(Number(document.forms[0].elements.angle.value)*Math.PI/180).toFixed(3));
-}
+  e.preventDefault();
+  document.forms[0].elements.total.value = `sin(${document.forms[0].elements.angle.value}°)=${Math.sin(Number(document.forms[0].elements.angle.value)*Math.PI/180).toFixed(3)}`;
+ }
