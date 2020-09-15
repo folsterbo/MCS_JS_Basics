21144a3e5ad2b5e7fca5a2e5b812993b43f852d9
folsterbo <folsterbo@gmail.com>
2020-09-15 23:03:54 +0300

    meteo update

--- 6_4_1_json_meteo/js/script.js
+++ 6_4_1_json_meteo/js/script.js
@@ -1,26 +1,23 @@
 let APIKey = 'fa8edf8c33760477099767f66bdda182';
-let city = 'Москва'; //document.forms[0].elements.place.value
-let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+ APIKey;
+let city = document.querySelector('input[name="place"]').value;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', url, false);
-xhr.send();
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
-if (xhr.status !=200) {
-  console.log(xhr.status + ' '+xhr.statusText);
-} else {
-  var DATA = JSON.parse(xhr.responseText);
-  console.log(DATA);
-}
-
-
-
-Object.values(document.forms[0])[2].onclick = function(e) {
+document.forms[0].onsubmit = function(e) {
   e.preventDefault();
-  document.forms[0].elements.temp.value = (DATA.main.temp - 273).toFixed(1) + '°C';
- }
-
- Object.values(document.forms[0])[3].onclick = function(e) {
-  e.preventDefault();
-  document.forms[0].elements.pressure.value = (DATA.main.pressure*0.750062).toFixed(0) + ' мм рт.ст.';
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false);
+  xhr.send();
+  if (xhr.status !=200) {
+    console.log(xhr.status + ' '+xhr.statusText);
+  } else {
+    var DATA = JSON.parse(xhr.responseText);
+    console.log(DATA);
+  }
+  document.querySelector('.temperature').innerHTML = (DATA.main.temp - 273).toFixed(0) + '°C';
+  document.querySelector('.pressure').innerHTML = 'Давление ' + (DATA.main.pressure*0.750062).toFixed(0) + ' мм рт. ст.';
+  document.querySelector('.feels-like').innerHTML = 'Ощущается как ' + (DATA.main.feels_like - 273).toFixed(0) + '°C';
+  document.querySelector('.wind').innerHTML = 'Скорость ветра ' + (DATA.wind.speed).toFixed(0) + ' м/с';
+  document.querySelector('.humidity').innerHTML = 'Влажность ' + (DATA.main.humidity).toFixed(0) + '%';
  }
