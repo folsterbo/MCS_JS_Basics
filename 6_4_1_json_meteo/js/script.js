9d33e6330f2125025b2a37abb7d87ce27464ec9e
folsterbo <folsterbo@gmail.com>
2020-09-16 10:30:36 +0300

    Update script.js

--- 6_4_1_json_meteo/js/script.js
+++ 6_4_1_json_meteo/js/script.js
@@ -1,11 +1,8 @@
-let APIKey = 'fa8edf8c33760477099767f66bdda182';
-let city = document.querySelector('input[name="place"]').value;
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-
 document.forms[0].onsubmit = function(e) {
   e.preventDefault();
+  let APIKey = 'fa8edf8c33760477099767f66bdda182';
+  let city = document.querySelector('input[name="place"]').value;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
   let xhr = new XMLHttpRequest();
   xhr.open('GET', url, false);
   xhr.send();
